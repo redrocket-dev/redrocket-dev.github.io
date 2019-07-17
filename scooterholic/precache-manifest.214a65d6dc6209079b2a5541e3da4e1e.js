5d40516a62d33d8a529a946af2678684ce1e4de8
redrocket-woonjang <50345321+redrocket-woonjang@users.noreply.github.com>
2019-07-17 10:04:32 +0900

    Add files via upload

---
+++ scooterholic/precache-manifest.214a65d6dc6209079b2a5541e3da4e1e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35c28aa404dabff843b3cbd0f324dd29",
+    "url": "/scooterholic/index.html"
+  },
+  {
+    "revision": "797efa4c23ca3429f9ad",
+    "url": "/scooterholic/static/css/main.d3647218.chunk.css"
+  },
+  {
+    "revision": "6dacec45fe0a46656c49",
+    "url": "/scooterholic/static/js/2.210345e0.chunk.js"
+  },
+  {
+    "revision": "797efa4c23ca3429f9ad",
+    "url": "/scooterholic/static/js/main.8ca52e2c.chunk.js"
+  },
+  {
+    "revision": "2c2e06f16fc0c45be616",
+    "url": "/scooterholic/static/js/runtime~main.efa3e0d5.js"
+  },
+  {
+    "revision": "dd1b8ed48b6ba1cd11fe071b09d3b9f1",
+    "url": "/scooterholic/static/media/about-cover.dd1b8ed4.jpg"
+  },
+  {
+    "revision": "023985ebf29fc4e32210857ec4e4d9d1",
+    "url": "/scooterholic/static/media/dropdown-arrow.023985eb.svg"
+  },
+  {
+    "revision": "1ae2f0db80c397cd4ed5298421c39dbb",
+    "url": "/scooterholic/static/media/hamburger.1ae2f0db.svg"
+  },
+  {
+    "revision": "0392d2775b03c1773e539e41fd6b4b30",
+    "url": "/scooterholic/static/media/landing-cover.0392d277.jpg"
+  }
+]);
